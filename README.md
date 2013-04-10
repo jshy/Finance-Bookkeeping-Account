61bc7ea62bed9d9a54fa103323cf14ac169bf98c
jshy <finn.cool@yahoo.com>
2013-04-10 16:01:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Finance::Bookkeeping::Account - Blah blah blah
+
+# SYNOPSIS
+
+    use Finance::Bookkeeping::Account;
+
+# DESCRIPTION
+
+Finance::Bookkeeping::Account is
+
+# AUTHOR
+
+Jesse Shy <jshy@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Jesse Shy
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Finance/Bookkeeping/Account.pm
@@ -0,0 +1,73 @@
+package Finance::Bookkeeping::Account;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+use MooseX::Role:Parameterized;
+
+parameter nb, isa => 'Str', required => 1;
+
+role {
+	my $self = shift;
+
+	if ($self->nb eq 'credit') {   # nb is 'normal balance'
+		
+		method debit => sub debit { 
+			my ($self, $amount) = @_;
+			$self->balance($self->balance - $amount); 
+		};
+
+		method credit => sub credit {
+			my ($self, $amount) = @_;
+			$self->balance($self->balance + $amount); 
+		};
+	}
+
+	if ($self->nb eq 'debit') {
+		method debit => sub debit { 
+			my ($self, $amount) = @_;
+			$self->balance($self->balance + $amount); 
+		};
+
+		method credit => sub credit {
+			my ($self, $amount) = @_;
+			$self->balance($self->balance - $amount); 
+		};
+	}
+
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Finance::Bookkeeping::Account - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Finance::Bookkeeping::Account;
+
+=head1 DESCRIPTION
+
+Finance::Bookkeeping::Account is
+
+=head1 AUTHOR
+
+Jesse Shy E<lt>jshy@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Jesse Shy
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
